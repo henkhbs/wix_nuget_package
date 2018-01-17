7187955c1575a36258e583300c36f7b28c044f50
henkhbs <henk@hbsbv.com>
2018-01-17 10:34:25 +0100

    added explaining article files

---
+++ Article/Automatically add references and content to the WIX installer _ Arjan Hordijk_files/rate.php
@@ -0,0 +1,6 @@
+PDRTJS_6780689_post_85.avg_rating = 5;
+PDRTJS_6780689_post_85.votes = 5;		
+PDRTJS_settings_6780689_post_85= {"type" : "stars","size" : "sml","star_color" : "yellow","custom_star" : "","font_size" : "","font_line_height" : "16px","font_color" : "","font_align" : "left","font_position" : "right","font_family" : "","font_bold" : "normal","font_italic" : "normal","text_vote" : "Vote","text_votes" : "Votes","text_rate_this" : "Rate This","text_1_star" : "Very Poor","text_2_star" : "Poor","text_3_star" : "Average","text_4_star" : "Good","text_5_star" : "Excellent","text_thank_you" : "Thank You","text_rate_up" : "Rate Up","text_rate_down" : "Rate Down"};
+PDRTJS_6780689_post_85.init();		
+PDRTJS_6780689_post_85.token='e2fcf342c9ef7fef30152b7058e4f309';
+/*6780689,_post_85,wp-post-85,3581787501,5-5*/
