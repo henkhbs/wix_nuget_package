348a9fdb8eb1fd2484c59048ddf87757e5a856a4
Henk Beekhuis <Henk@hbsbv.com>
2018-01-17 10:31:23 +0100

    Adding explaining article

---
+++ Article/Automatically add references and content to the WIX installer _ Arjan Hordijk_files/rate(3).php
@@ -0,0 +1,6 @@
+PDRTJS_6780691_comm_14.nero_up = 0;
+PDRTJS_6780691_comm_14.nero_dn = 0;		
+PDRTJS_settings_6780691_comm_14= {"type" : "nero","size" : "sml","star_color" : "hand","custom_star" : "","font_size" : "","font_line_height" : "16px","font_color" : "","font_align" : "left","font_position" : "right","font_family" : "","font_bold" : "normal","font_italic" : "normal","text_vote" : "Vote","text_votes" : "Votes","text_rate_this" : "Rate This","text_1_star" : "Very Poor","text_2_star" : "Poor","text_3_star" : "Average","text_4_star" : "Good","text_5_star" : "Excellent","text_thank_you" : "Thank You","text_rate_up" : "Rate Up","text_rate_down" : "Rate Down"};
+PDRTJS_6780691_comm_14.init();		
+PDRTJS_6780691_comm_14.token='85b5d3341690537c33831e9610a0f7ed';
+/*6780691,_comm_14,wp-comment-14,3581787501,0-0*/
